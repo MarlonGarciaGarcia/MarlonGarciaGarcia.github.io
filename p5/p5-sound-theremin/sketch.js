9bfd5626cd0880518f1bbb7b2b96658aeec7ca36
marlon garcia <marlon.garciagarcia@sjsu.edu>
2019-03-28 02:31:15 -0700

    Update sketch.js

--- p5/p5-sound-theremin/sketch.js
+++ p5/p5-sound-theremin/sketch.js
@@ -2,26 +2,27 @@
 
 // coding train sound synth tutorial https://www.youtube.com/watch?v=Bk8rLzzSink&t=0s&index=7&list=PLRqwX-V7Uu6aFcVjlDAkkGIixw70s7jpW
 
-let osc;
+let yeeee;
 
 function setup() {
   createCanvas(400, 400);
-  osc = new p5.Oscillator('square');
+  yeeee = new p5.Oscillator('square');
 
 }
 
 function draw() {
   background(220);
-  osc.freq(60 + mouseX);
-  osc.amp(map(mouseY, 0, height, 0.2, 0));
+  yeeee.freq(60 + mouseX);
+  yeeee.amp(map(mouseY, 0, height, 0.2, 0));
   // to draw shape
   // rect(50, 50, map(mouseX, 0, width, 0, 200), map(mouseY, 0, height, 0, 200));
 }
 
 function mousePressed() {
-  osc.start();
+  yeeee.start();
 }
 
 function mouseReleased() {
-  osc.stop();
+  yeeee.stop();
 }
+yeeee
